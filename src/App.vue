de7672c4f9f6e639f9e5968ce3ed30742e56fae2
zeinali <zeinali.b69@yahoo.com>
2022-01-08 16:25:41 +0330

    wix weather cols and add modal for adding city

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,17 @@
 <template>
-  <v-app>
+  <div class="main">
     <navigation />
-  </v-app>
+  </div>
 </template>
-
 <script>
-import axios from "axios";
-import db from "./firebase/firebaseinit";
+
 import Navigation from "./components/Navigation.vue";
 export default {
   name: "App",
   components: {
     Navigation,
   },
-  data() {
-    return {
-      APIkey: "c55dc8f793ea78f694e30d9af7413816",
-      cities: [],
-    };
-  },
-
-  methods: {
-    getCityWeather() {
-      let firebaseDB = db.collection("cities");
-      firebaseDB.onSnapshot((snap) => {
-        snap.docChanges().forEach(async (doc) => {
-          if (doc.type === "added") {
-            try {
-              const reponse = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${
-                  doc.doc.data().city
-                }&units=imperial&APPID=${this.APIkey}`
-              );
-              const data = reponse.data;
-              console.log(doc.doc.data());
-              firebaseDB
-                .doc(doc.doc.id)
-                .update({
-                  currentWeather: data,
-                })
-                .then(() => {
-                  this.cities.push(doc.doc.data());
-                })
-                .then(() => {
-                  console.log(this.cities);
-                });
-            } catch (err) {
-              console.log(err);
-            }
-          }
-        });
-      });
-    },
-  },
-  created() {
-    this.getCityWeather();
-  },
+  
 };
 </script>
 
